56cf1cc900d8463c8fb4e7aff0bab73ec9014f8a
uvsq22101593 <91332757+uvsq22101593@users.noreply.github.com>
2022-04-15 10:17:53 +0200

    Update projet.Mastermind.py

--- projet.Mastermind.py
+++ projet.Mastermind.py
@@ -29,16 +29,18 @@ bouton_quitter = tk.Button(racine, text="Quitter", bg ="white", command = fermer
 bouton_2joueurs = tk.Button(racine, text= "2 Joueurs", bg= "grey", command = mode_2joueurs)
 bouton_1joueur = tk.Button(racine, text= "1 Joueur", bg= "grey", command = mode_1joueur)
 
-rectangle10 = canvas.create_rectangle(150,60,350,100, fill = "white", outline="black")
-rectangle9 = canvas.create_rectangle(150,110,350,150, fill = "white", outline="black")
-rectangle8 = canvas.create_rectangle(150,160,350,200, fill = "white", outline="black")
-rectangle7 = canvas.create_rectangle(150,210,350,250, fill = "white", outline="black")
-rectangle6 = canvas.create_rectangle(150,260,350,300, fill = "white", outline="black")
-rectangle5 = canvas.create_rectangle(150,310,350,350, fill = "white", outline="black")
-rectangle4 = canvas.create_rectangle(150,360,350,400, fill = "white", outline="black")
-rectangle3 = canvas.create_rectangle(150,410,350,450, fill = "white", outline="black")
-rectangle2 = canvas.create_rectangle(150,460,350,500, fill = "white", outline="black")
-rectangle1 = canvas.create_rectangle(150,510,350,550, fill = "white", outline="black")
+
+rectangle10 = canvas.create_rectangle(150,10,350,50, fill = "grey", outline="black")
+rectangle9 = canvas.create_rectangle(150,60,350,100, fill = "grey", outline="black")
+rectangle8 = canvas.create_rectangle(150,110,350,150, fill = "grey", outline="black")
+rectangle7 = canvas.create_rectangle(150,160,350,200, fill = "grey", outline="black")
+rectangle6 = canvas.create_rectangle(150,210,350,250, fill = "grey", outline="black")
+rectangle5 = canvas.create_rectangle(150,260,350,300, fill = "grey", outline="black")
+rectangle4 = canvas.create_rectangle(150,310,350,350, fill = "grey", outline="black")
+rectangle3 = canvas.create_rectangle(150,360,350,400, fill = "grey", outline="black")
+rectangle2 = canvas.create_rectangle(150,410,350,450, fill = "grey", outline="black")
+rectangle1 = canvas.create_rectangle(150,460,350,500, fill = "grey", outline="black")
+rectangle0 = canvas.create_rectangle(150,560,350,600, fill = "grey", outline="black")
 
 
 
