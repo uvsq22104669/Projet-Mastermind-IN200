8ddc48bba84a3568c01e15ff3fb43fe8d97f7e42
uvsq22101593 <91332757+uvsq22101593@users.noreply.github.com>
2022-04-15 10:12:28 +0200

    Update projet.Mastermind.py

--- projet.Mastermind.py
+++ projet.Mastermind.py
@@ -29,6 +29,18 @@ bouton_quitter = tk.Button(racine, text="Quitter", bg ="white", command = fermer
 bouton_2joueurs = tk.Button(racine, text= "2 Joueurs", bg= "grey", command = mode_2joueurs)
 bouton_1joueur = tk.Button(racine, text= "1 Joueur", bg= "grey", command = mode_1joueur)
 
+rectangle10 = canvas.create_rectangle(150,60,350,100, fill = "white", outline="black")
+rectangle9 = canvas.create_rectangle(150,110,350,150, fill = "white", outline="black")
+rectangle8 = canvas.create_rectangle(150,160,350,200, fill = "white", outline="black")
+rectangle7 = canvas.create_rectangle(150,210,350,250, fill = "white", outline="black")
+rectangle6 = canvas.create_rectangle(150,260,350,300, fill = "white", outline="black")
+rectangle5 = canvas.create_rectangle(150,310,350,350, fill = "white", outline="black")
+rectangle4 = canvas.create_rectangle(150,360,350,400, fill = "white", outline="black")
+rectangle3 = canvas.create_rectangle(150,410,350,450, fill = "white", outline="black")
+rectangle2 = canvas.create_rectangle(150,460,350,500, fill = "white", outline="black")
+rectangle1 = canvas.create_rectangle(150,510,350,550, fill = "white", outline="black")
+
+
 
 # Position des widgets :
 bouton_quitter.grid(column=1, row=3)
@@ -37,5 +49,6 @@ bouton_1joueur.grid(column=1, row=1)
 
 
 
-racine. mainloop()
 
+
+racine. mainloop()
